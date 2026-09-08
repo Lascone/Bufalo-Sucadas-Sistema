da9c46df702008c924a181d71adcf3b32b766fcc
Lascone <magraoofficial@gmail.com>
2026-09-08 03:37:56 -0300

    chore: bump version to 0.1.29

--- packages/shared/src/constants.ts
+++ packages/shared/src/constants.ts
@@ -1,6 +1,6 @@
 export const APP_NAME = 'Búfalo Sucata Gestor';
 export const APP_COMPANY = 'Búfalo Sucatas';
-export const APP_VERSION = '0.1.28';
+export const APP_VERSION = '0.1.29';
 
 export const SyncStatus = {
   SYNCED: 'SYNCED',
